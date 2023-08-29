1ae6eed5a920df07e7a38b99ab7a2a4f58e1137e
Александр Лещинский <topjumbo@mail.ru>
2023-08-29 11:55:41 +1000

    поправил немногоAdd project files.

---
+++ SocialNetwork.Tests/UserNotFoundException.Test.cs
@@ -0,0 +1,15 @@
+﻿using SocialNetwork.BLL.Services;
+using SocialNetwork.BLL.Exceptions;
+
+namespace SocialNetwork.Tests
+{
+    [TestFixture]
+    public class SomeTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.Throws<UserNotFoundException>(() => new UserService().FindByEmail("ThisEmailShouldNotBeInDataBase@mail.ru"));
+        }
+    }
+}
